9e9c91e26e2093d935e27a3ab0678ffd2ef5701c
“PetyaDjanovska” <petya.djanovska@gmail.com>
2018-07-26 14:11:48 -0400

    Changed Campsite Description to text

---
+++ db/migrate/20180726180914_change_campsite_description_to_text.rb
@@ -0,0 +1,5 @@
+class ChangeCampsiteDescriptionToText < ActiveRecord::Migration[5.2]
+  def change
+    change_column :campsites, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_26_180309) do
+ActiveRecord::Schema.define(version: 2018_07_26_180914) do
 
   create_table "campsites", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
