9ec6b93d925c3bac6d4878ab681b3b0e1db82fac
“PetyaDjanovska” <petya.djanovska@gmail.com>
2018-07-30 17:29:27 -0400

    fixed user.new form, created user#show and shoe.html.erb, signup working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/users/:id', to: 'users#show'
+  get '/users/:id', to: 'users#show', as: 'user'
   get '/', to: 'application#home'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
