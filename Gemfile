e41604b50287e27077ac81bdab8ac572f0cfcc7c
“PetyaDjanovska” <petya.djanovska@gmail.com>
2018-07-30 16:20:10 -0400

    Created users and sessions controllers, users new and create routes, added has_secure_password to USer model

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ ruby '2.5.1'
 gem 'omniauth'
 gem "omniauth-google-oauth2", "~> 0.2.1"
 gem 'dotenv-rails'
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'application#home'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 end
