725c1e3c543421d02495057ecf3ca8f73fdbc70f
“PetyaDjanovska” <petya.djanovska@gmail.com>
2018-07-30 16:49:48 -0400

    added user.name validations; created user.new form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :name, uniqueness: true
+  validates :name, presence: true
+  
   has_many :favorites
   has_many :campsites, through: :favorites
-  has_secure_password
+  
 end
