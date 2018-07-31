b9c366759d932315080f43d9ae1fc9d82ef3b344
“PetyaDjanovska” <petya.djanovska@gmail.com>
2018-07-30 23:33:39 -0400

    Login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    if !params[:name].blank?
+      @user = User.find_by(name: params[:name])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        redirect_to '/'
+      end
+    end
   end
 end
