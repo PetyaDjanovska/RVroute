4d4383629df3365830116ac7fb3131e733ed59ff
“PetyaDjanovska” <petya.djanovska@gmail.com>
2018-07-31 13:52:17 -0400

    Google login and signup working!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,30 @@ class SessionsController < ApplicationController
   def new
   end
 
+
   def create
-    if !params[:name].blank?
-      @user = User.find_by(name: params[:name])
-      if @user && @user.authenticate(params[:password])
+    if auth_hash = request.env['omniauth.auth']
+      
+#       Existing user
+      if @user = User.find_by(uid: auth_hash['uid'])
         session[:user_id] = @user.id
+        
         redirect_to user_path(@user)
       else
-        redirect_to '/'
+        @user = User.new(uid: auth_hash['uid'], name: auth_hash['info']['first_name'], password: SecureRandom.hex)
+        if @user.save
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        end
       end
+    else
+      @user = User.find_by(name: params[:name])
+        if @user && @user.authenticate(params[:password])
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else 
+          redirect_to '/'
+        end
     end
   end
   
