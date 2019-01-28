05ebe762c869f82ddb34348b8754f8107ccaf591
“PetyaDjanovska” <petya.djanovska@gmail.com>
2019-01-28 14:37:08 -0500

    add  constructor and form

---
+++ app/assets/javascripts/campsite.js
@@ -0,0 +1,29 @@
+class Campsite {
+    constructor(obj){
+        this.name = obj.name;
+        this.address = obj.address;
+        this.description = obj.decription;
+    }
+
+    static campForm() {
+        // let campForm = (`
+        // <form id='new-camp-form'>
+        //     <!--name -->
+        //     <div>
+        //         <input type="text" placeholder="name">
+        //     </div>
+
+        //     <!--address -->
+        //     <div>
+        //         <input type="text" placeholder="address">
+        //     </div>
+
+        //     <!--description -->
+        //     <div>
+        //         <input type="text" placeholder="description">
+        //     </div>
+
+        // </form>
+        // `)
+    }
+}
