3037732b13a571375651c6b03159bbced07d6642
“PetyaDjanovska” <petya.djanovska@gmail.com>
2019-02-07 18:01:46 -0500

    improve on the campsite object

--- app/assets/javascripts/campsite.js
+++ app/assets/javascripts/campsite.js
@@ -1,8 +1,9 @@
 class Campsite {
     constructor(obj){
+        this.id = obj.id;
         this.name = obj.name;
         this.address = obj.address;
-        this.description = obj.decription;
+        this.description = obj.description;
     }
 
     static campForm() {
@@ -26,4 +27,47 @@ class Campsite {
         // </form>
         // `)
     }
+}
+
+Campsite.prototype.campHTML = function () {
+	return (`
+		<div>
+			<h3 class="item" id="${this.id}">${this.name}</h3>
+			<p>${this.address}</p>
+			<p>${this.description}</p>
+		</div>
+		<br>
+	`)
+}
+
+function listCamps() {
+    const url = baseUrl + "campsites";
+    const ul =  $('#fetch-data');
+
+    fetch(url)
+    .then (response => response.json())
+    .then(data => {
+        console.log(data);
+        let camps = data;
+        return camps.map(obj => {
+        let currentCamp = new Campsite(obj);
+        var parser = new DOMParser();
+        var html = parser.parseFromString(currentCamp.campHTML(), 'text/html');    
+        ul.append(html.body.firstChild);
+        })
+    })
+}
+
+function showCamp(id) {
+    let url = baseUrl + "campsites/" + id;
+    const ul =  $('#fetch-data');
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        let currentCamp = new Campsite(data);
+        var parser = new DOMParser();
+        var html = parser.parseFromString(currentCamp.campHTML(), 'text/html');    
+        ul.append(html.body.firstChild);
+    })
 }
