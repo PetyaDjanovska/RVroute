91e719bc0edb2bb038493bbbc32cfeed7ea38be0
“PetyaDjanovska” <petya.djanovska@gmail.com>
2019-02-07 18:02:28 -0500

    display list of campsites and an individual campsite

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,9 +1,31 @@
 $(function() {
-    attachListeners();
+    listenForCamps();
 })
 
-function attachListeners () {
-    $('#camps').click(function() {
-        alert('I was clicked')
-    })
+
+const baseUrl = 'http://localhost:3000/';
+
+function clearData() {
+    $('#fetch-data').empty();
 }
+
+function listenForCamps () {
+    clearData();
+    console.log('clearData was called')
+
+    $('#camps').on('click', function() {
+        listCamps();
+    })
+
+    // event delegation, so that an event handler can be attached to the newly created element
+    $(function () {
+        $('div').on('click', '.item', function(e){
+            clearData();
+           console.log(e.target.id);
+           showCamp(e.target.id);
+        });
+    });
+}
+
+
+
